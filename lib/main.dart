9997f91b6fc4284ebdbfcddf18f3685f273a1f2f
N1ghtler <beevadhana@gmail.com>
2022-07-12 18:56:23 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+
+import 'package:quickfood/pages/auth/login/login_page.dart';
+import 'package:quickfood/pages/food_details/food_detail.dart';
 import 'package:quickfood/pages/my_home.dart';
+import 'package:quickfood/pages/auth/register/register_page.dart';
 import 'package:quickfood/routes/routes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().whenComplete(() => print("LOADED"));
   runApp(const MyApp());
 }
 
@@ -11,10 +19,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
       routes: Routes.routes,
     );
   }
